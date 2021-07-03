b98d5d2a40286e98f250b11e7ec940e1e21f775c
AndrewLester <alester220@gmail.com>
2021-07-03 15:52:56 -0400

    Fix main.js link

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,6 @@ const navItems = document.querySelectorAll('nav a');
 for (let i = 0; i < navItems.length; i++) {
     if (navItems[i].href.endsWith(window.location.pathname)) {
         navItems[i].className = 'active';
+        break;
     }
 }
